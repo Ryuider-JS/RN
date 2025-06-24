7f6b1c850f1f5d2ab0ad7645a7967ca6598d90be
지승 <rjs8833@gmail.com>
2025-06-24 14:03:12 +0900

    feat: Text & TextInput

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import {Text} from 'react-native';
+import './global.css';
+
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TextInput, View } from 'react-native';
 
 const App = () => {
-  return <Text>1</Text>;
+  const [title, setTitle] = useState('');
+
+  return (
+    <SafeAreaView>
+      <View className="flex items-center gap-2 p-4 pt-8 bg-white">
+        <Text className="h-10">{title}</Text>
+        <TextInput
+          className="w-full border-2 rounded-md"
+          onChangeText={(text: string) => {
+            setTitle(text);
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default App;
