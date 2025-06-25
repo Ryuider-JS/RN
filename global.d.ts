32f76a763d87d3a41e8a95bcd277ab0edf95267e
지승 <rjs8833@gmail.com>
2025-06-25 14:36:09 +0900

    chore: supabase setting

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+// env 추가시 타입 선언
+
+declare module '@env' {
+  export const SUPABASE_URL: string;
+  export const SUPABASE_ANON_KEY: string;
+}
---
+++ src/lib/supabase.ts
@@ -0,0 +1,25 @@
+import 'react-native-url-polyfill/auto';
+
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@env';
+import { createClient, processLock } from '@supabase/supabase-js';
+
+import { AppState } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    lock: processLock,
+  },
+});
+
+AppState.addEventListener('change', state => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
