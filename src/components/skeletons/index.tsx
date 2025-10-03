599e527bc4d1c5be060906b12b9ec85c8576dfda
지승 <rjs8833@gmail.com>
2025-10-03 20:15:46 +0900

    chore: apply assets/fonts

--- src/components/skeletons/index.tsx
+++ src/components/skeletons/index.tsx
@@ -4,7 +4,7 @@ const Skeletons = () => {
   return (
     <SkeletonPlaceholder borderRadius={4}>
       <SkeletonPlaceholder.Item flexDirection="row" alignItems="center">
-        <SkeletonPlaceholder.Item width={60} height={60} borderRadius={48} />
+        <SkeletonPlaceholder.Item width={56} height={56} borderRadius={48} />
         <SkeletonPlaceholder.Item marginLeft={20}>
           <SkeletonPlaceholder.Item width={120} height={20} />
           <SkeletonPlaceholder.Item marginTop={6} width={80} height={20} />
--- src/screens/Profile/components/ProfileLogin/ProfileLoginCommon.tsx
+++ src/screens/Profile/components/ProfileLogin/ProfileLoginCommon.tsx
@@ -1,10 +1,19 @@
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 import { IProfileLoginCommonProps } from '@/types/profile.type';
 
 const ProfileLoginCommon = ({ data }: IProfileLoginCommonProps) => {
-  console.log(data);
-  return data ? <View /> : <Text>2</Text>;
+  return data ? (
+    <View className="flex-row items-center">
+      <Image
+        source={{ uri: data.profileImageUrl ?? '' }}
+        className="rounded-full size-14"
+      />
+      <Text>{data.name}</Text>
+    </View>
+  ) : (
+    <Text>2</Text>
+  );
 };
 
 export default ProfileLoginCommon;
